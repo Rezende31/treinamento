2d7a70ac145da65dd8f3905d8fb8fc9ded5ad09c
Rezende31 <84739439+Rezende31@users.noreply.github.com>
2022-01-25 11:17:31 -0300

    fazendo um teste com o git

--- src/treinamento/controller/PedidoController.java
+++ src/treinamento/controller/PedidoController.java
@@ -1,9 +1,13 @@
 package treinamento.controller;
 
+import java.text.SimpleDateFormat;
+
 public class PedidoController {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		SimpleDateFormat teste = new SimpleDateFormat("dd-MM-yyyy");
+		System.out.println("teste");
 
 	}
 
